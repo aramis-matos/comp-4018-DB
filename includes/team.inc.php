95abcce043da2c5a5bb3cce2fef88b17ede960a6
aramis-matos <aramis.matos1@gmail.com>
2022-05-12 13:19:34 -0400

    added calculated value for team

--- includes/team.inc.php
+++ includes/team.inc.php
@@ -22,7 +22,22 @@ if (isset($_POST['insert'])) {
 }
 
 else if (isset($_POST['view'])) {
-    view($name, $conn);
+    // view($name, $conn);
+
+    $query = "SELECT * FROM $name;";
+    $result = mysqli_query($conn, $query);
+    $result_num = mysqli_num_rows($result);
+    if ($result_num > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $query = "SELECT university.name FROM university WHERE university.id = " . $row['uni_id'];
+            $new_result = mysqli_query($conn,$query);
+            echo "Team Name: " . mysqli_fetch_assoc($new_result)['name'] . "_" . $row['est_1'] . $row['est_2'] . $row['est_3'] . "_" . date('Y', strtotime($row['year_participated'])) . " ";
+            foreach ($row as $key => $val) {
+                echo "$key: $val ";
+            }
+            echo "<br>";
+        }
+    }
 }
 
 else if (isset($_POST['modify'])) {
