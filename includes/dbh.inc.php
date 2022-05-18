e8c9ccc6d91f6e9c4d2a6200c009c1f58f456ad0
aramis-matos <aramis.matos1@gmail.com>
2022-05-18 11:11:02 -0400

    changed login info

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
+$dbUsername = "aramismn";
+$dbPassword = "aramismn";
 $dbName = "comateq001";
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
